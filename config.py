be1e958e7e5c0162d75b741c2de77f5ee42358ea
Shadow <jujutsukaigojosaturo@gmail.com>
2025-05-07 09:10:52 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+API_ID = "29478593"
+API_HASH = "24c3a9ded4ac74bab73cbe6dafbc8b3e"
+BOT_TOKEN = "8029266331:AAGQao17r6ykXi8UTP2QS6MBwrlAwk5mFNA"
+MONGO_URI = "mongodb+srv://nitinkumardhundhara:DARKXSIDE78@cluster0.wdive.mongodb.net/?retryWrites=true&w=majority"
+
+SESSION_NAME = "AutoIndex"
+START_PIC = "https://wallpapers.com/images/featured/miku-nakano-h9l1l1gmrv07fpmf.jpg"
+FORCE_PIC = "https://wallpapercave.com/wp/wp10524495.jpg"
+
+OWNER_ID = 7086472788
+ADMINS = [7096258348]
+FORCE_SUB_CHANNEL = "0"
